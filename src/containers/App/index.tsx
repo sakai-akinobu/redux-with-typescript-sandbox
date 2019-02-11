2833f18704db813a505b4bb72277ce7b77e24562
sakai-akinobu <sakai.akinobu@gmail.com>
2019-02-11 14:13:05 +0900

    Connect to redux store.

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,21 +1,50 @@
 import * as React from 'react';
-import {useState} from 'react';
+import {Dispatch} from 'redux';
+import {connect} from 'react-redux';
 
+import {
+  increment,
+  decrement,
+} from '../../modules/counters';
 import Counter from '../../components/Counter';
 import Button from '../../components/Button';
 
-export default function App() {
-  const [count, setCount] = useState(0);
+interface Props {
+  count: number,
+  actions: {
+    increment: () => void,
+    decrement: () => void,
+  },
+}
+
+function App(props: Props) {
+  const {
+    count,
+    actions,
+  } = props;
 
   return (
     <div>
-      <Button onClick={() => setCount(count - 1)}>
+      <Button onClick={actions.decrement}>
         -
       </Button>
       <Counter value={count} />
-      <Button onClick={() => setCount(count + 1)}>
+      <Button onClick={actions.increment}>
         +
       </Button>
     </div>
   );
 }
+
+const mapStateToProps = (state: any) => ({
+  count: state.counters.count,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  actions: {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
